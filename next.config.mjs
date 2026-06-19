1d0464af39beeabe78724de096211165164a2dcc
trungticket <45954824+trungticket@users.noreply.github.com>
2026-06-19 07:56:40 +0000

    Allow Codespaces origin for Server Actions

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        "*.app.github.dev",
+        "*.githubpreview.dev",
+        "localhost:3000",
+        "127.0.0.1:3000",
+      ],
+    },
+  },
+};
+
 export default nextConfig;
